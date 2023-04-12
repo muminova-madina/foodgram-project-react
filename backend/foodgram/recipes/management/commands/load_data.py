943857359d99b0b7ce551357eedb235c4892c1b6
madina <madi92@mail.ru>
2023-04-12 22:41:40 +0300

    fixed bags

--- backend/foodgram/recipes/management/commands/load_data.py
+++ backend/foodgram/recipes/management/commands/load_data.py
@@ -1,43 +1,22 @@
-import csv
-import sys
+import json
 
-from django.core.management.base import BaseCommand
+from django.core.management import BaseCommand
 
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Импорт данных из csv в модель Ingredient'
-
-    def add_arguments(self, parser):
-        parser.add_argument('--path', type=str, help='Путь к файлу')
 
     def handle(self, *args, **options):
-        params_for_create = []
+        with open(
+                '../../data/ingredients.json',
+                encoding='utf-8') as f:
+            ingredients = json.load(f)
 
-        try:
-            file_path = options['path']
-            with open(file_path, 'r', encoding='utf-8') as csv_file:
-                reader = csv.reader(csv_file)
-                for row in reader:
-                    #unit_obj, created = Ingredient.objects.get_or_create(name=row[1])
-                    params = {
-                        'name': row[0],
-                        'measurement_unit': row[1],
-                    }
-                    params_for_create.append(params)
-
-            Ingredient.objects.bulk_create(
-                [Ingredient(**parameters) for parameters in params_for_create]
+        for ingredient in ingredients:
+            name = ingredient['name']
+            measurement_unit = ingredient['measurement_unit']
+            Ingredient.objects.create(
+                name=name,
+                measurement_unit=measurement_unit
             )
-        except Exception as error:
-            self.stdout.write(
-                self.style.ERROR(f'Error loading model {error}'),
-            )
-            sys.exit()
-
-        self.stdout.write(
-            self.style.SUCCESS(
-                f'{len(params_for_create)}' ' Ingredient Objects Created'
-            )
-        )

b4e1f4fe34c7eab9cff01a3ca8819e9eb6e6952d
madina <madi92@mail.ru>
2023-04-27 00:25:12 +0300

    final

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -119,11 +119,11 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError({
                     'amount': 'Количество ингредиента должно быть больше 0!'
                 })
-            elif int(amount) > MAX_VALUE:
+            if int(amount) > MAX_VALUE:
                 raise serializers.ValidationError({
                     'amount': 'Количество ингредиента должно быть меньше 32000!'
                 })
-            elif element['id'] in ingredients_data:
+            if element['id'] in ingredients_data:
                 raise serializers.ValidationError({
                     'ingredient': 'Ингредиенты должны быть уникальными!'
                 })
